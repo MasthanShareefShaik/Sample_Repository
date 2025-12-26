536b42b746bec21785e0b018a21a0d9d6ed4a79c
masthan shareef <shareefshaik3162@gmail>
2025-12-26 09:28:38 +0530

    Initial project commit

---
+++ sts_check_Backend/src/test/java/com/example/sts_check/StsCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sts_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
