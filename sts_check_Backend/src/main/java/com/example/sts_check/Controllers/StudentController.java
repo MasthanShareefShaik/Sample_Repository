384c5352ba39e9005969a399d972c189f715c724
masthan shareef <shareefshaik3162@gmail>
2026-01-03 11:44:21 +0530

    Added RestController in StudentController.java

--- sts_check_Backend/src/main/java/com/example/sts_check/Controllers/StudentController.java
+++ sts_check_Backend/src/main/java/com/example/sts_check/Controllers/StudentController.java
@@ -1,5 +1,11 @@
 package com.example.sts_check.Controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+	
 
 }
