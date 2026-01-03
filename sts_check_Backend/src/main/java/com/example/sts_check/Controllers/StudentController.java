3afa46616c5dbf0ad54580fbde40d1e8d7bfd0e6
masthan shareef <shareefshaik3162@gmail>
2026-01-03 11:42:22 +0530

    Created controller package

---
+++ sts_check_Backend/src/main/java/com/example/sts_check/Controllers/StudentController.java
@@ -0,0 +1,5 @@
+package com.example.sts_check.Controllers;
+
+public class StudentController {
+
+}
--- sts_check_Backend/src/main/java/com/example/sts_check/Entity/Test.java
+++ sts_check_Backend/src/main/java/com/example/sts_check/Entity/Test.java
@@ -1,5 +1,26 @@
 package com.example.sts_check.Entity;
 
 public class Test {
+private Integer id;
+private String testName;
+private Integer marks;
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getTestName() {
+	return testName;
+}
+public void setTestName(String testName) {
+	this.testName = testName;
+}
+public Integer getMarks() {
+	return marks;
+}
+public void setMarks(Integer marks) {
+	this.marks = marks;
+}
 
 }
