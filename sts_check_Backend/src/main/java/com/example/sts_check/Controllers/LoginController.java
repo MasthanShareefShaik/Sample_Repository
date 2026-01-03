8ef34765eec554a522d2ea131d38abd1defd10aa
masthan shareef <shareefshaik3162@gmail>
2026-01-03 12:04:20 +0530

    created saveLogin method

--- sts_check_Backend/src/main/java/com/example/sts_check/Controllers/LoginController.java
+++ sts_check_Backend/src/main/java/com/example/sts_check/Controllers/LoginController.java
@@ -1,10 +1,15 @@
 package com.example.sts_check.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/login")
+
 public class LoginController {
+	@GetMapping("/login")
+	public String saveLogin() {
+		return "User login was success";
+	}
 
 }
