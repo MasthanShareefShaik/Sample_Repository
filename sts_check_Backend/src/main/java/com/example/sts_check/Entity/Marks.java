08a2e2049768f1c9b4cd400d4d95de96393ef7fa
masthan shareef <shareefshaik3162@gmail>
2026-01-24 15:45:55 +0530

    added marks entity

---
+++ sts_check_Backend/src/main/java/com/example/sts_check/Entity/Marks.java
@@ -0,0 +1,27 @@
+package com.example.sts_check.Entity;
+
+public class Marks {
+	private Integer id;
+	private Integer marks;
+	private String grade;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getMarks() {
+		return marks;
+	}
+	public void setMarks(Integer marks) {
+		this.marks = marks;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	
+
+}
